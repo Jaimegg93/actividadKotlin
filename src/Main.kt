95c4938385ddaf72cde00380cab49f2d04809ab5
jaimegg93 <jaimegg93@educastur.es>
2024-11-25 17:49:02 +0100

    Ejercicio1 acabado

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+    for(i in 1 ..  100){
+        if((i %3 == 0)&&(i%5 == 0)){
+            println("triqui")
+        }else if(i %5 == 0){
+            println("qui")
+        }else if((i %3 == 0)){
+            println("tri")
+        }else{
+            println(i)
+        }
+    }
 }
